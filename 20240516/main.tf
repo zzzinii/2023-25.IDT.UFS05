49b6b5213f87021d9914b2282e90fcd812af31d0
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-05-14 01:17:08 +0000

    fix: tf az virtual network

--- 20240516/main.tf
+++ 20240516/main.tf
@@ -19,3 +19,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "<sandbox created resource group uuid>"
   location = "westus"
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
