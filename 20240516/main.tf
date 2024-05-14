13326df47fe30c1a86b04047c25a3a367ee46a15
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-05-14 01:17:29 +0000

    fix: tf az subnet

--- 20240516/main.tf
+++ 20240516/main.tf
@@ -26,3 +26,10 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
